bf8c5122ced5e637b7a3b032cc87ad346d2ae229
Rick-Xia <danteveqann@gmail.com>
2018-07-07 14:15:58 -0400

    update readme

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -12,20 +12,6 @@ const determineOpacity = ( combi, wind, src ) => {
     return 1
 }
 
-const determineBorderColor = ( wind, src ) => {
-    if ( wind ) {
-        if ( src.startsWith(wind) ) return "green"
-    }
-    return "inherit"
-}
-
-const determineBorderStyle = ( wind, src ) => {
-    if ( wind ) {
-        if ( src.startsWith(wind) ) return "solid"
-    }
-    return "none"
-}
-
 const Tile = ( props ) => {
     const { onClick, src, ...rest } = props
 
@@ -33,9 +19,7 @@ const Tile = ( props ) => {
         <Grid item>
             <input type="image"
                     className="Tile"
-                    style={{ opacity: determineOpacity(rest.combi, rest.wind, src),
-                            borderColor: determineBorderColor( rest.wind, src),
-                            borderStyle: determineBorderStyle( rest.wind, src ) }}
+                    style={{ opacity: determineOpacity(rest.combi, rest.wind, src) }}
                     
                     onClick={onClick}
                     src={`./img/${src}.png`}
